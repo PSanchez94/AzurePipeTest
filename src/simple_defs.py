55da789ba2bac9024a1f22815e2c7b1ed2f8ed38
PSanchez94 <pablo.sanchez.r@ug.uchile.cl>
2020-01-09 15:57:21 -0300

    Deleted the test folder and the src python script

--- src/simple_defs.py
+++
@@ -1,4 +0,0 @@
-# A PyTest for Azure Pipeline
-
-def simple_sum(x, y):
-    return x + y
--- test/simple_test.py
+++
@@ -1,9 +0,0 @@
-from simple_defs import simple_sum
-
-
-def test_simplesum():
-    assert simple_sum(1, 1) == 2
-
-
-def test_failingsum():
-    assert not simple_sum(1, 1) == 3
